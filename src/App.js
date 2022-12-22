ae8df147c7b1d72b909df2644e33dccea3bf2dee
ebenyoub <ebenyoub@me.com>
2022-12-22 16:19:49 +0100

    2nd deploy 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Games from './components/Games/Games';
